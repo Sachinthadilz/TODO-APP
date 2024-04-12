1558e5df27641a934854674b4210fcede6c65736
Sachintha Dilshan <it22587510@my.sliit.lk>
2024-04-12 18:14:02 +0530

    Update mongodb.js

--- init/mongodb.js
+++ init/mongodb.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const connectionUrl = "mongodb+srv://sachinthadilshan:abcd1234@itppro1.d6u6lyh.mongodb.net/todo_app?retryWrites=true&w=majority";
-
 const connectMongodb = async()=>{
     try {
         await mongoose.connect(process.env.CONNECTION_URL);
